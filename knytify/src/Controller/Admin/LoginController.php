6a8d8628df1d200c242e1f6776b52d6cf2cee6b2
JF <betterask.jf@gmail.com>
2022-12-04 15:22:30 +0100

    Created UI for both registration and login

--- knytify/src/Controller/Admin/LoginController.php
+++ knytify/src/Controller/Admin/LoginController.php
@@ -7,26 +7,24 @@ namespace Knytify\Controller\Admin;
 
 use Symfony\Component\HttpFoundation\Request;
 use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 // use Knytify\Service\Admin\LoginForm;
 
 use Knytify\Entity\Admin\LoginEntity;
 
-
-
 class LoginController extends FrameworkBundleAdminController
 {
     public function indexAction(Request $request)
     {
-        // $form = new LoginForm($this);
-
-        // creates a logindata and gives it some dummy data for this example
         $login = new LoginEntity();
 
         $form = $this->createFormBuilder($login)
             ->add('username', TextType::class)
-            ->add('password', TextType::class)
-            ->add('save', SubmitType::class, ['label' => 'Log In'])
+            ->add('password', PasswordType::class)
+            ->add('save', SubmitType::class, ['label' => 'Associate account'])
             ->getForm();
 
         return $this->render(
--- knytify/src/Controller/Admin/RegistrationController.php
+++ knytify/src/Controller/Admin/RegistrationController.php
@@ -1,15 +1,37 @@
 <?php
 
+
+
 namespace Knytify\Controller\Admin;
 
+
+use Symfony\Component\HttpFoundation\Request;
 use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+
+use Knytify\Entity\Admin\RegistrationEntity;
 
 class RegistrationController extends FrameworkBundleAdminController
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+        $entity = new RegistrationEntity();
+
+        $form = $this->createFormBuilder($entity)
+            ->add('username', TextType::class)
+            ->add('password', PasswordType::class)
+            ->add('passwordCheck', PasswordType::class)
+            ->add('save', SubmitType::class, ['label' => 'Register account'])
+            ->getForm();
+
         return $this->render(
             '@Modules/knytify/views/templates/admin/registration.html.twig',
+            [
+                'form' => $form->createView()
+            ]
         );
     }
 }
