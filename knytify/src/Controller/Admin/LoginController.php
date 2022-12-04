dcd1f6d60590546eecef81d16d872978e728860d
JF <betterask.jf@gmail.com>
2022-12-04 14:50:33 +0100

    Fix Admin/Entity -> Entity/Admin

--- knytify/src/Controller/Admin/LoginController.php
+++ knytify/src/Controller/Admin/LoginController.php
@@ -1,15 +1,39 @@
 <?php
 
+
+
 namespace Knytify\Controller\Admin;
 
+
+use Symfony\Component\HttpFoundation\Request;
 use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
+
+// use Knytify\Service\Admin\LoginForm;
+
+use Knytify\Entity\Admin\LoginEntity;
+
+
 
 class LoginController extends FrameworkBundleAdminController
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+        // $form = new LoginForm($this);
+
+        // creates a logindata and gives it some dummy data for this example
+        $login = new LoginEntity();
+
+        $form = $this->createFormBuilder($login)
+            ->add('username', TextType::class)
+            ->add('password', TextType::class)
+            ->add('save', SubmitType::class, ['label' => 'Log In'])
+            ->getForm();
+
         return $this->render(
             '@Modules/knytify/views/templates/admin/login.html.twig',
+            [
+                'form' => $form->createView()
+            ]
         );
     }
 }
