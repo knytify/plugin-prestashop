098851e0cbed61692b44b09ae26a7f12d4f58c81
JFK <jean-francois.kener@lemonads.com>
2023-02-12 17:55:26 +0100

    fix bugs

--- knytify/_dev/src/views/configuration/Configuration.vue
+++ knytify/_dev/src/views/configuration/Configuration.vue
@@ -1,21 +1,25 @@
 <template>
   <div>
-    <div class="mb-3" v-if="billingContext.user.email">
-      <Header controller="KnytifyConfiguration" />
-      <v-card>
-        <v-tabs>
-          <v-tab @click="tab = 'general'">General</v-tab>
-          <v-tab @click="tab = 'utm'">UTM source</v-tab>
-          <v-tab @click="tab = 'subscription'">Subscription</v-tab>
-        </v-tabs>
-        <General v-if="tab == 'general'" />
-        <UTM v-else-if="tab == 'utm'" />
-        <Subscription v-if="tab == 'subscription'" />
-      </v-card>
-    </div>
+    <div v-if="psAccount.user.email">
+      <div class="mb-3" v-if="psBillingContext.user.email">
+        <Header controller="KnytifyConfiguration" />
+        <v-card>
+          <v-tabs>
+            <v-tab @click="tab = 'general'">General</v-tab>
+            <v-tab @click="tab = 'utm'">UTM source</v-tab>
+            <v-tab @click="tab = 'subscription'">Subscription</v-tab>
+          </v-tabs>
+          <General v-if="tab == 'general'" />
+          <UTM v-else-if="tab == 'utm'" />
+          <Subscription
+            v-if="tab == 'subscription'"
+          />
+        </v-card>
+      </div>
 
-    <div v-else>
-      <Subscription />
+      <div v-else>
+        <Subscription />
+      </div>
     </div>
   </div>
 </template>
@@ -29,12 +33,14 @@ import Subscription from "./Subscription.vue";
 
 export default {
   name: "App",
+  created() {},
   data: function () {
+    console.log("PS Account", window.contextPsAccounts);
+    console.log("PS Billing", window.psBillingContext.context);
     return {
       tab: "general",
-      billingContext: { ...window.psBillingContext.context },
-      modalType: "",
-      sub: null,
+      psAccount: { ...window.contextPsAccounts },
+      psBillingContext: { ...window.psBillingContext.context },
     };
   },
   components: {
@@ -42,45 +48,6 @@ export default {
     General,
     UTM,
     Subscription,
-  },
-  provide() {
-    return {
-      emailSupport: window.psBillingContext.context.user.emailSupport,
-    };
-  },
-  methods: {
-    openBillingModal(type, data) {
-      this.modalType = type;
-      this.billingContext = { ...this.billingContext, ...data };
-    },
-    closeBillingModal(data) {
-      this.modalType = "";
-      this.$refs.psBillingCustomerRef.parent.updateProps({
-        context: {
-          ...this.billingContext,
-          ...data,
-        },
-      });
-    },
-    eventHookHandler(type, data) {
-      switch (type) {
-        case EVENT_HOOK_TYPE.BILLING_INITIALIZED:
-          // data structure is: { customer, subscription }
-          console.log("Billing initialized", data);
-          this.sub = data.subscription;
-          break;
-        case EVENT_HOOK_TYPE.SUBSCRIPTION_UPDATED:
-          // data structure is: { customer, subscription, card }
-          console.log("Sub updated", data);
-          this.sub = data.subscription;
-          break;
-        case EVENT_HOOK_TYPE.SUBSCRIPTION_CANCELLED:
-          // data structure is: { customer, subscription }
-          console.log("Sub cancelled", data);
-          this.sub = data.subscription;
-          break;
-      }
-    },
   },
 };
 </script>
--- knytify/_dev/src/views/configuration/Subscription.vue
+++ knytify/_dev/src/views/configuration/Subscription.vue
@@ -22,25 +22,57 @@ import {
 } from "@prestashopcorp/billing-cdc/dist/bundle.umd";
 import { Vue } from "vue";
 
-
 export default {
   data() {
     return {
-      form: {
-        utm: {},
-      },
+      billingContext: { ...window.psBillingContext.context },
+      modalType: "",
+      sub: null,
+    };
+  },
+  provide() {
+    return {
+      emailSupport: window.psBillingContext.context.user.emailSupport,
     };
   },
   components: {
     PsBillingCustomer: CustomerComponent.driver("vue3", Vue),
     PsBillingModal: ModalContainerComponent.driver("vue3", Vue),
   },
-  watch: {
-    init() {
-      this.reset();
+  methods: {
+    openBillingModal(type, data) {
+      this.modalType = type;
+      this.billingContext = { ...this.billingContext, ...data };
+    },
+    closeBillingModal(data) {
+      this.modalType = "";
+      this.$refs.psBillingCustomerRef.parent.updateProps({
+        context: {
+          ...this.billingContext,
+          ...data,
+        },
+      });
+    },
+    eventHookHandler(type, data) {
+      switch (type) {
+        case EVENT_HOOK_TYPE.BILLING_INITIALIZED:
+          // data structure is: { customer, subscription }
+          console.log("Billing initialized", data);
+          this.sub = data.subscription;
+          break;
+        case EVENT_HOOK_TYPE.SUBSCRIPTION_UPDATED:
+          // data structure is: { customer, subscription, card }
+          console.log("Sub updated", data);
+          this.sub = data.subscription;
+          break;
+        case EVENT_HOOK_TYPE.SUBSCRIPTION_CANCELLED:
+          // data structure is: { customer, subscription }
+          console.log("Sub cancelled", data);
+          this.sub = data.subscription;
+          break;
+      }
     },
   },
-  methods: {},
-  name: "Configuration",
+  name: "Subscription",
 };
 </script>
